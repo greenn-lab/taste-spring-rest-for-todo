d41aaaab7c34886871f3049819de7aeea5d1fccf
green <greenn.lab@gmail.com>
2021-05-12 22:31:28 +0900

    update//불필요한 파일 지우고 CommandLineRunner 로 바꿈

--- src/main/java/study/tastespringrest/TasteSpringRestApplication.java
+++ src/main/java/study/tastespringrest/TasteSpringRestApplication.java
@@ -1,10 +1,29 @@
 package study.tastespringrest;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import study.tastespringrest.todo.Todo;
+import study.tastespringrest.todo.TodoRepository;
 
 @SpringBootApplication
-public class TasteSpringRestApplication {
+@RequiredArgsConstructor
+public class TasteSpringRestApplication implements CommandLineRunner {
+
+  private final TodoRepository repository;
+
+
+  // CommandLineRunner 가 안되는게 아니었어요.
+  // 아마도 log level 이 높아서 로깅 해본게 안나온 것일 뿐인것 같아요.
+  @Override
+  public void run(String... args) throws Exception {
+    Todo todo = new Todo();
+    todo.setTitle("hello!");
+    todo.setCompleted(true);
+
+    repository.save(todo);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TasteSpringRestApplication.class, args);
